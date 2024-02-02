afc1800ce575c8bfed857e76673e85b3efbc3213
jeremias-skip <jchinchayp@gmail.com>
2024-02-02 01:06:34 -0500

    actualizando
    actua

--- js/questions.js
+++ js/questions.js
@@ -1,14 +1,14 @@
 (function(){
     const titleQuestion= [...document.querySelectorAll('.questions__title')];
 console.log(titleQuestion);
-titleQuestion.forEach(question =>{
+titleQuestion.forEach(questions =>{
 
-question.addEventListener('click', ()=>{
+questions.addEventListener('click', ()=>{
     let height = 0;
-    let answer = question.nextElementSibling;
-    let addPadding = question.parentElement.parentElement;
+    let answer = questions.nextElementSibling;
+    let addPadding = questions.parentElement.parentElement;
     addPadding.classList.toggle('questions__padding--add');
-    question.children[0].classList.toggle('question__arrow--rotate');
+    questions.children[0].classList.toggle('questions__arrow--rotate');
         if(answer.clienteHeight===0){
         height = answer.scrollHeight;
         }
